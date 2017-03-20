e65acf845f2f6bbb92fe301db0418c1f813e6c18
kapoorabhish <abhishek.kapoor@outlook.com>
2017-03-20 14:29:54 +0530

    completed basic functionality of posting issues, need to catch gitlab exceptions

--- app/taiga_gitlab/controllers.py
+++ app/taiga_gitlab/controllers.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, Response, request
 import json
-import configparser
-import os
 import logging
 from app import app
+import gitlab
+import traceback
 
-logging.basicConfig(level=logging.DEBUG)
+LOG_FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
+logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
 logger = logging.getLogger(__name__)
 
 taiga_gitlab = Blueprint('taiga_gitlab', __name__, url_prefix="/taiga-gitlab")
@@ -17,35 +18,40 @@ def gitlab_view():
         return Response("It's working!!!.")
 
     if request.method == 'POST':
+        response_dict = {}
         incoming_data = json.loads(request.data.decode('utf-8'))
-        logger.debug(incoming_data)
+        gitlab_project_id = request.args.get("gitlab_project_id")
+        gitlab_access_token = app.config.get("GITLAB_PERSONAL_ACCESS_TOKEN")
+        gitlab_url = app.config.get("GITLAB_URL")
+
         if "type" in incoming_data:
             if incoming_data["type"] == "userstory" and incoming_data["action"] == "create":
-                taiga_project_id = incoming_data["data"]["project"]["id"]
-
-                projects_config_file_path = app.config.get("PROJECTS_CONFIG_FILE_PATH")
-
-                if projects_config_file_path:
-                    if os.path.isfile(projects_config_file_path):
-                        projects_config = configparser.ConfigParser()
-                        projects_config.read(projects_config_file_path)
-                        if "taiga_gitlab_projects_id_mapping" in projects_config:
-                            projects_mapping = projects_config["taiga_gitlab_projects_id_mapping"].get("mappings")
-                            projects_mapping_dict = {}
-                            for each_mapping in projects_mapping.split(";"):
-                                each_mapping_splitted = each_mapping.split(":")
-                                if len(each_mapping_splitted) == 2:
-                                    projects_mapping_dict[int(each_mapping_splitted[0])] = int(each_mapping_splitted[1])
-
-                            if taiga_project_id in projects_mapping_dict:
-                                logging.debug(taiga_project_id, projects_mapping_dict)
-
+                if gitlab_access_token and gitlab_url:
+                    if gitlab_project_id:
+                        story_subject = incoming_data["data"]["subject"]
+                        story_description = incoming_data["data"]["description"]
+                        gl = gitlab.Gitlab(gitlab_url, gitlab_access_token)
+                        try:
+                            # gl_project = gl.projects.get(gitlab_project_id)
+                            gl_issue = gl.project_issues.create({'title': story_subject, 'description': story_description}, project_id=gitlab_project_id)
+                        except Exception as e:
+                            response_dict["error"] = "Could not add issue in gitlab"
+                            response_dict["error_trace"] = traceback.format_stack()
+                            logger.error(e)
                         else:
-                            logger.error("No taiga_gitlab_projects_id_mapping section defined in {0} ".format(projects_config_file_path))
-
+                            response_dict["success"] = True
                     else:
-                        logger.error("No projects config file path found")
-        logger.debug(incoming_data)
-        return Response("It's working!!!.")
+                        response_dict["error"] = "Gitlab project ID not provided in WEBHOOK URL."
+                else:
+                    response_dict["error"] = "Gitlab access token or gitlab url not configured."
+
+
+        response = app.response_class(
+            response=json.dumps(response_dict),
+            status=200,
+            mimetype='application/json'
+        )
+
+        return response
 
     return Response()
--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
 
-PROJECTS_CONFIG_FILE_NAME = "projects_config.txt"
-PROJECTS_CONFIG_FILE_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),"projects_config.txt")
+GITLAB_PERSONAL_ACCESS_TOKEN = ""
+GITLAB_URL = ""
+
+try:
+    from local_config import *
+except:
+    pass
